fb3933a7a7cbeff265a8cf40bbcc56260fddeef0
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-03 15:42:22 +0500

    Add functions

---
+++ index.js
@@ -0,0 +1,29 @@
+// Define numbers
+let num1 = 8;
+let num2 = 2;
+// Getting elements using Dom manipulation
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+let sumEl = document.getElementById("sum-el");
+let showEntries = document.getElementById("showEntries");
+
+// Create add(),subtract(),divide(),multiply() functions
+function add() {
+    let result = num1 + num2;
+    sumEl.textContent = "Sum: " + result;
+}
+
+function subtract() {
+    let result = num1 - num2;
+    sumEl.textContent = "Sum: " + result;
+}
+
+function divide() {
+    let result = num1 / num2;
+    sumEl.textContent = "Sum: " + result;
+}
+
+function multiply() {
+    let result = num1 * num2;
+    sumEl.textContent = "Sum: " + result;
+}
